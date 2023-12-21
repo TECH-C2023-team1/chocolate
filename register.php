c0648ff95475682b3a6a375cb56db734eab04982
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2023-12-21 14:45:55 +0900

    add roguin

--- register.php
+++ register.php
@@ -110,6 +110,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>    
             <div class="form-group">
                 <label>パスワード</label>
+                <p>8文字以上入力して下さい</p>
                 <input type="password" name="password" class="form-control <?php echo (!empty(h($errors['password']))) ? 'is-invalid' : ''; ?>" value="<?php echo h($datas['password']); ?>">
                 <span class="invalid-feedback"><?php echo h($errors['password']); ?></span>
             </div>
