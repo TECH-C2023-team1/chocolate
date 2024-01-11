51752ce46013753171734295e10aa1057a1d6795
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2024-01-11 13:27:42 +0900

    add roguin

--- location_selection.php
+++ location_selection.php
@@ -11,7 +11,7 @@ const DB_PASSWORD = '';
 const DB_NAME = 'location';
 
 // データベースへの接続
-$conn = new mysqli(DB_HOST, DB_NAME, DB_PASSWORD, DB_USER);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 // 接続エラーチェック
 if ($conn->connect_error) {
