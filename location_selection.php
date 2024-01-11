96fe2adec803132b91488cc505b12e369853c211
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2024-01-11 13:52:39 +0900

    add roguin

--- location_selection.php
+++ location_selection.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>データ入力フォーム</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 </head>
 <body>
     <h1>データ入力フォーム</h1>
@@ -23,6 +25,11 @@
     <p>選択した場所: <?php echo $selectedOption; ?></p>
 
     <a href="welcome.php" class="btn btn-primary">戻る</a>
-
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </body>
 </html>
