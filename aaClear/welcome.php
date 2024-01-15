d439d96cce94433a09a6ca4c2276bcfe86fb4a0b
FCAmiyamoto <FCA22050011@edu.fca.ac.jp>
2024-01-15 12:30:53 +0900

    add CSS

--- aaClear/welcome.php
+++ aaClear/welcome.php
@@ -18,6 +18,8 @@ $username = $_SESSION['username'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../CSS/welcome.css">
 </head>
 <body>
     <h2>Welcome, <?php echo $username; ?>!</h2>
