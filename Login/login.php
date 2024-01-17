726d5c47450e1543aec2facc4fcccb9b6813c88b
OgawaShuhei <fca22060004@edu.fca.ac.jp>
2024-01-17 22:57:48 +0900

    add roguin

--- Login/login.php
+++ Login/login.php
@@ -37,7 +37,7 @@ $conn->close();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
